4df4945cc993a16b7358618d6ffd0529a88efd20
P13L0v3r <pie.lover8547@live.com>
2024-04-19 23:02:16 -0500

    hhh

--- startup.js
+++ startup.js
@@ -1,31 +1,29 @@
-$(document).ready(function () {
-    $(".custom-button").hover(
-        function () {
-            $(this).removeClass("raised");
-        },
-        function () {
-            $(this).addClass("raised");
-            $(this).removeClass("pressed");
-        }
-    );
-    $(".custom-button")
-        .on("mousedown", function () {
-            $(this).addClass("pressed");
-        })
-        .on("mouseup", function () {
-            $(this).removeClass("pressed");
-        });
-    $(".custom-button")
-        .on("click", function () {
-            var href = $(this).data("href");
-            window.location.href = href;
-        });
-    $("td").each(function () {
-        var parent = $(this).parent();
-        var rowspan = $(this).attr("rowspan") ? parseInt($(this).attr("rowspan")) : 1;
-        if (rowspan + parent.index() == parent.parent().children().length) {
-            $(this).addClass("bottom-row");
-        }
+$(".custom-button").hover(
+    function () {
+        $(this).removeClass("raised");
+    },
+    function () {
+        $(this).addClass("raised");
+        $(this).removeClass("pressed");
+    }
+);
+$(".custom-button")
+    .on("mousedown", function () {
+        $(this).addClass("pressed");
+    })
+    .on("mouseup", function () {
+        $(this).removeClass("pressed");
     });
-    $(".hide-until-load").show();
+$(".custom-button")
+    .on("click", function () {
+        var href = $(this).data("href");
+        window.location.href = href;
+    });
+$("td").each(function () {
+    var parent = $(this).parent();
+    var rowspan = $(this).attr("rowspan") ? parseInt($(this).attr("rowspan")) : 1;
+    if (rowspan + parent.index() == parent.parent().children().length) {
+        $(this).addClass("bottom-row");
+    }
 });
+$(".hide-until-load").show();
