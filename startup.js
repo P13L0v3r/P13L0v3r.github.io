74011e4980c75d3997dfc9f7a63320d78c4a7e0b
P13L0v3r <pie.lover8547@live.com>
2024-04-19 22:45:59 -0500

    Trying something new

--- startup.js
+++ startup.js
@@ -1,38 +1,40 @@
 $(document).ready(function () {
-    $(".snippet").each(function () {
+    $.when($(".snippet").each(function () {
         var snippetToReplace = $(this);
         var href = snippetToReplace.data("href");
         $.get(href, function (data) {
             snippetToReplace.replaceWith(data);
         })
-    });
-    $(".custom-button").hover(
-        function () {
-            $(this).removeClass("raised");
-        },
-        function () {
-            $(this).addClass("raised");
-            $(this).removeClass("pressed");
-        }
-    );
-    $(".custom-button")
-        .on("mousedown", function () {
-            $(this).addClass("pressed");
-        })
-        .on("mouseup", function () {
-            $(this).removeClass("pressed");
+    })).then(function () {
+        $(".custom-button").hover(
+            function () {
+                $(this).removeClass("raised");
+            },
+            function () {
+                $(this).addClass("raised");
+                $(this).removeClass("pressed");
+            }
+        );
+        $(".custom-button")
+            .on("mousedown", function () {
+                $(this).addClass("pressed");
+            })
+            .on("mouseup", function () {
+                $(this).removeClass("pressed");
+            });
+        $(".custom-button")
+            .on("click", function () {
+                var href = $(this).data("href");
+                window.location.href = href;
+            });
+        $("td").each(function () {
+            var parent = $(this).parent();
+            var rowspan = $(this).attr("rowspan") ? parseInt($(this).attr("rowspan")) : 1;
+            if (rowspan + parent.index() == parent.parent().children().length) {
+                $(this).addClass("bottom-row");
+            }
         });
-    $(".custom-button")
-        .on("click", function () {
-            var href = $(this).data("href");
-            window.location.href = href;
-        });
-    $("td").each(function () {
-        var parent = $(this).parent();
-        var rowspan = $(this).attr("rowspan") ? parseInt($(this).attr("rowspan")) : 1;
-        if (rowspan + parent.index() == parent.parent().children().length) {
-            $(this).addClass("bottom-row");
-        }
+        $(".hide-until-load").show();
     });
-    $(".hide-until-load").show();
+
 });
