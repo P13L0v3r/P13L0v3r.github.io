b8cfa92202f6348bfdf884fa291d2914c280de46
P13L0v3r <pie.lover8547@live.com>
2024-04-19 22:49:47 -0500

    Last try

--- startup.js
+++ startup.js
@@ -1,40 +1,39 @@
+$(".snippet").each(function () {
+    var snippetToReplace = $(this);
+    var href = snippetToReplace.data("href");
+    $.get(href, function (data) {
+        snippetToReplace.replaceWith(data);
+    })
+});
 $(document).ready(function () {
-    $.when($(".snippet").each(function () {
-        var snippetToReplace = $(this);
-        var href = snippetToReplace.data("href");
-        $.get(href, function (data) {
-            snippetToReplace.replaceWith(data);
+    
+    $(".custom-button").hover(
+        function () {
+            $(this).removeClass("raised");
+        },
+        function () {
+            $(this).addClass("raised");
+            $(this).removeClass("pressed");
+        }
+    );
+    $(".custom-button")
+        .on("mousedown", function () {
+            $(this).addClass("pressed");
         })
-    })).then(function () {
-        $(".custom-button").hover(
-            function () {
-                $(this).removeClass("raised");
-            },
-            function () {
-                $(this).addClass("raised");
-                $(this).removeClass("pressed");
-            }
-        );
-        $(".custom-button")
-            .on("mousedown", function () {
-                $(this).addClass("pressed");
-            })
-            .on("mouseup", function () {
-                $(this).removeClass("pressed");
-            });
-        $(".custom-button")
-            .on("click", function () {
-                var href = $(this).data("href");
-                window.location.href = href;
-            });
-        $("td").each(function () {
-            var parent = $(this).parent();
-            var rowspan = $(this).attr("rowspan") ? parseInt($(this).attr("rowspan")) : 1;
-            if (rowspan + parent.index() == parent.parent().children().length) {
-                $(this).addClass("bottom-row");
-            }
+        .on("mouseup", function () {
+            $(this).removeClass("pressed");
         });
-        $(".hide-until-load").show();
+    $(".custom-button")
+        .on("click", function () {
+            var href = $(this).data("href");
+            window.location.href = href;
+        });
+    $("td").each(function () {
+        var parent = $(this).parent();
+        var rowspan = $(this).attr("rowspan") ? parseInt($(this).attr("rowspan")) : 1;
+        if (rowspan + parent.index() == parent.parent().children().length) {
+            $(this).addClass("bottom-row");
+        }
     });
-
+    $(".hide-until-load").show();
 });
